3b1e495312223770bc923d355db52181ece5e356
unknown <jatinsharma6204063696@gmail.com>
2023-05-10 22:47:39 +0530

    full stack

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ function Intro() {
     <Wrapper id='intro'>
       <div className="intro">
       <div className="intro-content">
-        <p id='introText'>Hey There, I'm <span className='main'>JATIN SHARMA </span>, Third Year CSE Undergrad and a <span className='main'>FRONT-END WEB DEVELOPER</span>  at BIT MESRA</p>
+        <p id='introText'>Hey There, I'm <span className='main'>JATIN SHARMA </span>, Third Year CSE Undergrad and a <span className='main'>FULL-STACK WEB DEVELOPER</span>  at BIT MESRA</p>
       </div>
       <div className="my-img">
         <img src={myImg} alt='my'/>
