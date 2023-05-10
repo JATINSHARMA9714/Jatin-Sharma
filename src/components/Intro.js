9f42b03fb2d0f275807427ea844ffc4c1c45be2c
unknown <jatinsharma6204063696@gmail.com>
2023-05-10 22:45:38 +0530

    photo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Wrapper from '../wrapper/Intro'
-import myImg from '../assets/my_img.jfif'
+import myImg from '../assets/myImg.jpg'
 
 function Intro() {
   return (
@@ -10,7 +10,7 @@ function Intro() {
         <p id='introText'>Hey There, I'm <span className='main'>JATIN SHARMA </span>, Third Year CSE Undergrad and a <span className='main'>FRONT-END WEB DEVELOPER</span>  at BIT MESRA</p>
       </div>
       <div className="my-img">
-        <img src={myImg} alt="my image" />
+        <img src={myImg} alt='my'/>
       </div>
       </div>
     </Wrapper>
